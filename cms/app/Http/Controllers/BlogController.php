64be0f42b7d1b7bd1ac849b08f0323911bc98720
jefferson marin <jeffersondariom@gmail.com>
2021-06-15 23:01:36 -0500

    122, 123 Actualizar redes sociales del Gestor Blog

--- cms/app/Http/Controllers/BlogController.php
+++ cms/app/Http/Controllers/BlogController.php
@@ -28,7 +28,8 @@ class BlogController extends Controller
     					"servidor"=>$request->input("servidor"),
     					"titulo"=>$request->input("titulo"),
     					"descripcion"=>$request->input("descripcion"),
-                        "palabras_claves"=>$request->input("palabras_claves")
+                        "palabras_claves"=>$request->input("palabras_claves"),
+                        "redes_sociales"=>$request->input("redes_sociales")
                     );
         // Validar los datos
     	if(!empty($datos)){
@@ -38,7 +39,8 @@ class BlogController extends Controller
     			"servidor" => 'required|regex:/^[-\\_\\:\\.\\0-9a-z]+$/i',
     			"titulo" => 'required|regex:/^[0-9a-zA-ZñÑáéíóúÁÉÍÓÚ ]+$/i',
     			"descripcion" => 'required|regex:/^[=\\&\\$\\;\\-\\_\\*\\"\\<\\>\\?\\¿\\!\\¡\\:\\,\\.\\0-9a-zA-ZñÑáéíóúÁÉÍÓÚ ]+$/i',
-                "palabras_claves" => 'required|regex:/^[,\\0-9a-zA-ZñÑáéíóúÁÉÍÓÚ ]+$/i'
+                "palabras_claves" => 'required|regex:/^[,\\0-9a-zA-ZñÑáéíóúÁÉÍÓÚ ]+$/i',
+                "redes_sociales" => 'required'
                 ]);
             // Revisar la validacion
             if($validar->fails()){
@@ -51,6 +53,7 @@ class BlogController extends Controller
                                     "titulo" => $datos["titulo"],
                                     "descripcion" => $datos["descripcion"],
                                     "palabras_claves" => json_encode(explode(",", $datos["palabras_claves"])),
+                                    "redes_sociales" => $datos["redes_sociales"]
                                 );
                 $blog=BLOG::where("id",$id)->update($actualizar);
                 //echo "ok";
