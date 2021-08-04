3211892d2689e04656b53b65f0b3df26a2d68f8a
jefferson marin <jeffersondariom@gmail.com>
2021-08-03 22:40:06 -0500

    133. Modal para editar Usuario-Administrador

--- cms/app/Http/Controllers/AdministradoresController.php
+++ cms/app/Http/Controllers/AdministradoresController.php
@@ -8,9 +8,34 @@ use App\Blog;
 
 class AdministradoresController extends Controller
 {
+    	/*=============================================
+	Mostrar todos los registros
+	=============================================*/
     public function index(){
         $administradores=Administradores::all();
         $blog=Blog::all();
         return view("paginas.administradores",array("administradores"=>$administradores,"blog"=>$blog));
     }
+
+
+
+    /*=============================================
+	Mostrar un solo registro
+	=============================================*/
+
+	public function show($id){
+
+		$administrador = Administradores::where("id", $id)->get();
+		$blog = Blog::all();
+		$administradores = Administradores::all();
+
+		if(count($administrador) != 0){
+
+			return view("paginas.administradores", array("status"=>200, "administrador"=>$administrador, "blog"=>$blog, "administradores"=>$administradores));
+		
+		}else{ 
+
+			return view("paginas.administradores", array("status"=>404, "blog"=>$blog, "administradores"=>$administradores));
+		}
+	}
 }
