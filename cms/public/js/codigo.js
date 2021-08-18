2f343808c6afe5e726876bbfa109c272078f7f3d
jefferson marin <jeffersondariom@gmail.com>
2021-08-17 20:25:35 -0500

    150. Evitar repetir Ruta de Categoria

--- cms/public/js/codigo.js
+++ cms/public/js/codigo.js
@@ -343,4 +343,32 @@ $(document).on("keyup", ".inputRuta", function(){
 })
 
 
+/*=============================================
+Evitar repetir ruta 
+=============================================*/
+
+$(document).on("change",".inputRuta", function(){
+
+	$(".alert").remove();
+
+	var valorRuta = $(this).val();
+	var validarRuta = $(".validarRuta");
+
+	for(var i = 0; i < validarRuta.length; i++){
+
+		 if($(validarRuta[i]).html() == valorRuta){
+
+		 	 $(".inputRuta").val("");
+		 	 $(".inputRuta").parent().after(`
+
+				<div class="alert alert-danger">¡Error! Esta ruta ya existe en la base de datos</div>	
+
+		 	 `)
+
+		 }
+
+	}
+
+})
+
 
